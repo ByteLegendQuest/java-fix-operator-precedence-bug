c7858486038af65a3b692f5db58c1b101de5e5ff
cdwc233 <2241830325@outlook.com>
2023-03-05 12:40:55 +0000

    Update at 2023-03-05T12:40:55.354592Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,6 +13,9 @@ public class Challenge {
      * as empty string (`""`).
      */
     public static String concatString(String a, String b) {
-        return a == null ? "" : a + b == null ? "" : b;
+        if (a == null && b == null) return "";
+        else if (a == null) return b;
+        else if (b == null) return a;
+        else return a + b;
     }
 }
